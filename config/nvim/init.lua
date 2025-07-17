9eee7b3f751a7a4d23d73a0342842b68f9ad79c2
treasonx <treasonx@gmail.com>
2025-07-17 08:09:59 -0700

    Switch to lazy vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,2 @@
--- Load core configuration modules
-require("config.settings")    -- Core Neovim settings
-require("config.keymaps")     -- Global keymaps
-require("config.autocmds")    -- Autocommands
-require("config.lazy")        -- Plugin manager
-
--- Load theme synchronization
-require('config.theme-sync').setup()
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
